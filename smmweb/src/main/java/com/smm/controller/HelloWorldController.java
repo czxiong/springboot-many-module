8b89ac7ffa65287a7158af801136a1de57f932bc
czxiong <247859013@qq.com>
2018-04-18 15:03:48 +0800

    添加logbck.xml日志配置文件

--- smmweb/src/main/java/com/smm/controller/HelloWorldController.java
+++ smmweb/src/main/java/com/smm/controller/HelloWorldController.java
@@ -4,15 +4,20 @@ import com.smm.enums.ResultEnum;
 import com.smm.util.CustomException;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloWorldController {
 
     @GetMapping("/hello")
     @ApiOperation("hello world")
     public String hello(){
+        log.debug("记录debugger日志");
+        log.info("访问了hello方法");
+        log.error("记录error错误日志");
         return "Hello Spring Boot Module";
     }
 
